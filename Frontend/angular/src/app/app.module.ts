2fe8ce84f333f882849d74018c715623b3a7cf68
Eugeniu Nicolenco <clipstard@mail.ru>
2019-09-18 12:12:57 +0300

    [FIRST COMMIT]  - added project into docker;  - added project translations;  - created sample menu item;

---
+++ Frontend/angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/header/app-header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        AppHeaderComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        NgbModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (HttpLoaderFactory),
+                deps: [HttpClient]
+            }
+        })
+    ],
+    providers: [HttpClient],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # hire_yourself
+
+##
+
+create files **docker-compose.yml** from __dist__ file
+create files **src/environments/environment.ts** from __dist__ file
+create files **src/environments/environment.prod.ts** from __dist__ file
+
+configure your environment files 
+
+##
+
+```
+docker network create \
+        --subnet=172.38.0.0/16 \
+        --ip-range=172.38.5.0/24 \
+        --gateway=172.38.0.1 \
+        hirey-network
+
+docker-compose up
+```
+
+
+
+##
+### Access
+access pages via [172.38.0.2:4200](https://172.38.0.2:4200) or [localhost:4200](https://localhost:4200)
